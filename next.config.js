322eb9ff760763a42d1329b1b5d56bf8bf9977ab
pragyananda <82259244+pragyananda@users.noreply.github.com>
2024-08-15 19:08:59 +0530

    update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   export: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
